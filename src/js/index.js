c734015169561c13f3d25bbefb1cfa20b1d58564
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-10-25 10:58:47 -0400

    adicionei código js

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('')
+  elemento.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 100 * i)
+  });
+}
+
+
+const nome = document.querySelector(".my-name")
+typeWriter(nome)
